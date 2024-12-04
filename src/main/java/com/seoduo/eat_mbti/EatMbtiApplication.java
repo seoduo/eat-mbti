263942b5dd1ea289f5a403aba7de7a048b94ca9d
Jgone2 <jgoneit@gmail.com>
2024-12-04 16:22:23 +0900

    :tada:: 프로젝트 생성

---
+++ src/main/java/com/seoduo/eat_mbti/EatMbtiApplication.java
@@ -0,0 +1,13 @@
+package com.seoduo.eat_mbti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatMbtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatMbtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seoduo/eat_mbti/EatMbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoduo.eat_mbti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatMbtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
